2c12ba6eb94ab1a614e91f6523ac761181563d63
HERandall <59966283+HERandall@users.noreply.github.com>
2023-04-17 12:13:56 +0100

    Update config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# Set up Instructions for Fresh Pi OS, install all into the init.d folder.
+# 
+# apt install sudo
+# sudo apt install  python3
+#                   -y python3-picamera2
+#                   git
+#                   flex
+#                   bison
+#                   make
+#                   -y ffmpeg
+#                   time
+# pip3 install gps
+#                   
+# sudo apt-get install gpsd gpsd-clients
+#
+# curl https://get.pimoroni.com/bme680 | bash
+#
+# sudo raspi-config # # Enable Glamour in Pi Settings
+#
+# git pull origin main # # Updates code
+#
